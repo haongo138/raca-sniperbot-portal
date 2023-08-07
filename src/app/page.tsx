11869d93eac31ce8c3f3339e237bf239c7d04d10
Hao Ngo <haoxray@gmail.com>
2023-08-07 20:17:54 +0700

    chore: update env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,110 +5,13 @@ import { Popover, Transition } from '@headlessui/react'
 import { ChevronDownIcon, FingerPrintIcon } from '@heroicons/react/24/solid'
 import { StarIcon, ShieldCheckIcon, RocketLaunchIcon, BellAlertIcon, BookOpenIcon } from '@heroicons/react/24/outline'
 
-const features = [
-  {
-    name: 'Private Transactions',
-    description: 'Shield your trades, outsmart sandwich attacks.',
-    href: '##',
-    icon: FingerPrintIcon,
-  },
-  {
-    name: 'Limit Orders',
-    description: 'Buy low, sell high. Execute your strategy with precision.',
-    href: '##',
-    icon: StarIcon,
-  },
-  {
-    name: 'Early Contract Insights',
-    description: 'Test your sales ahead and maximize your selling potential.',
-    href: '##',
-    icon: ShieldCheckIcon,
-  },
-  {
-    name: 'Copy Trading',
-    description: 'Trade like the pros, front-run & replicate their wins.',
-    href: '##',
-    icon: RocketLaunchIcon,
-  },
-]
-
-const community = [
-  {
-    name: 'Updates',
-    href: '##',
-    icon: BellAlertIcon,
-  },
-  {
-    name: 'Blog (Coming Soon)',
-    href: '##',
-    icon: BookOpenIcon,
-  },
-]
-
-const socials = [
-  {
-    name: 'Twitter',
-    href: '##',
-    icon: IconOne,
-  },
-  {
-    name: 'Telegram',
-    href: '##',
-    icon: IconTwo,
-  },
-  {
-    name: 'Gitbook',
-    href: '##',
-    icon: IconThree,
-  },
-]
-
-function IconOne() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6 text-orange-500"
-      fill="currentColor"
-      viewBox="0 0 24 24">
-      <path
-        d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-    </svg>
-  )
-}
-
-function IconTwo() {
-  return (
-    <svg
-      className="h-6 w-6 text-orange-500"
-      fill="currentColor"
-      viewBox="0 0 24 24"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      xmlSpace="preserve"
-      style={{fillRule:"evenodd", clipRule:"evenodd", strokeLineJoin:"round", strokeMiterLimit:"1.41421" } as React.CSSProperties}>
-      <path
-        id="telegram-1"
-        d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z" />
-    </svg>
-
-  )
-}
-
-function IconThree() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6 text-orange-500"
-      fill="currentColor"
-      viewBox="0 0 24 24">
-      <path
-        d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-    </svg>
-  )
-}
-
 export default function Home() {
+  const CONTRACT = process.env.CONTRACT ?? "0x0000000"
+  const TWITTER = process.env.TWITTER ?? "##"
+  const TELEGRAM = process.env.TELEGRAM ?? "##"
+  const BOT_LINK = process.env.BOT ?? "##"
+  const BUY_LINK = "https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=" + CONTRACT + "&chain=mainnet"
+  
   useEffect(() => {
     const init = async () => {
       const { Datepicker, Input, initTE } = await import("tw-elements")
@@ -116,7 +19,110 @@ export default function Home() {
     }
     init()
   }, [])
-  
+
+  const features = [
+    {
+      name: 'Private Transactions',
+      description: 'Shield your trades, outsmart sandwich attacks.',
+      href: '##',
+      icon: FingerPrintIcon,
+    },
+    {
+      name: 'Limit Orders',
+      description: 'Buy low, sell high. Execute your strategy with precision.',
+      href: '##',
+      icon: StarIcon,
+    },
+    {
+      name: 'Early Contract Insights',
+      description: 'Test your sales ahead and maximize your selling potential.',
+      href: '##',
+      icon: ShieldCheckIcon,
+    },
+    {
+      name: 'Copy Trading',
+      description: 'Trade like the pros, front-run & replicate their wins.',
+      href: '##',
+      icon: RocketLaunchIcon,
+    },
+  ]
+
+  const community = [
+    {
+      name: 'Updates',
+      href: '##',
+      icon: BellAlertIcon,
+    },
+    {
+      name: 'Blog (Coming Soon)',
+      href: '##',
+      icon: BookOpenIcon,
+    },
+  ]
+
+  const socials = [
+    {
+      name: 'Twitter',
+      href: TWITTER,
+      icon: TwitterIcon,
+    },
+    {
+      name: 'Telegram',
+      href: TELEGRAM,
+      icon: TelegramIcon,
+    },
+    {
+      name: 'Gitbook (Coming soon)',
+      href: '##',
+      icon: GitbookIcon,
+    },
+  ]
+
+  function TwitterIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 text-orange-500"
+        fill="currentColor"
+        viewBox="0 0 24 24">
+        <path
+          d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+      </svg>
+    )
+  }
+
+  function TelegramIcon() {
+    return (
+      <svg
+        className="h-6 w-6 text-orange-500"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        xmlSpace="preserve"
+        style={{ fillRule: "evenodd", clipRule: "evenodd", strokeLineJoin: "round", strokeMiterLimit: "1.41421" } as React.CSSProperties}>
+        <path
+          id="telegram-1"
+          d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z" />
+      </svg>
+
+    )
+  }
+
+  function GitbookIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 text-orange-500"
+        fill="currentColor"
+        viewBox="0 0 24 24">
+        <path
+          d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+      </svg>
+    )
+  }
+
   return (
     <main className="overflow-hidden relative">
       <div className="absolute left-0 z-0 w-full min-h-[80vh] aspect-auto top-[-80px]">
@@ -268,7 +274,7 @@ export default function Home() {
         <span className="font-semibold text-white">Telegram Community Prize</span>
         <span className="flex items-center py-1 px-3 text-xs font-semibold text-black uppercase rounded-full" style={{ background: "linear-gradient(90deg, #F4C4C2 0%, #EEC3FD 48.96%, #8FC6E4 100%)" }}>
           <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" className="mr-1 w-4 iconify iconify--heroicons" width="1em" height="1em" viewBox="0 0 20 20">
-            <path fill="currentColor" fill-rule="evenodd" d="M10 1c-1.828 0-3.623.149-5.371.435a.75.75 0 0 0-.629.74v.387c-.827.157-1.642.345-2.445.564a.75.75 0 0 0-.552.698a5 5 0 0 0 4.503 5.152a6 6 0 0 0 2.946 1.822A6.451 6.451 0 0 1 7.768 13H7.5A1.5 1.5 0 0 0 6 14.5V17h-.75C4.56 17 4 17.56 4 18.25c0 .414.336.75.75.75h10.5a.75.75 0 0 0 .75-.75c0-.69-.56-1.25-1.25-1.25H14v-2.5a1.5 1.5 0 0 0-1.5-1.5h-.268a6.453 6.453 0 0 1-.684-2.202a6 6 0 0 0 2.946-1.822a5 5 0 0 0 4.503-5.152a.75.75 0 0 0-.552-.698A31.804 31.804 0 0 0 16 2.562v-.387a.75.75 0 0 0-.629-.74A33.227 33.227 0 0 0 10 1ZM2.525 4.422C3.012 4.3 3.504 4.19 4 4.09V5c0 .74.134 1.448.38 2.103a3.503 3.503 0 0 1-1.855-2.68Zm14.95 0a3.503 3.503 0 0 1-1.854 2.68A5.974 5.974 0 0 0 16 5v-.91c.496.099.988.21 1.475.332Z" clip-rule="evenodd"></path>
+            <path fill="currentColor" fillRule="evenodd" d="M10 1c-1.828 0-3.623.149-5.371.435a.75.75 0 0 0-.629.74v.387c-.827.157-1.642.345-2.445.564a.75.75 0 0 0-.552.698a5 5 0 0 0 4.503 5.152a6 6 0 0 0 2.946 1.822A6.451 6.451 0 0 1 7.768 13H7.5A1.5 1.5 0 0 0 6 14.5V17h-.75C4.56 17 4 17.56 4 18.25c0 .414.336.75.75.75h10.5a.75.75 0 0 0 .75-.75c0-.69-.56-1.25-1.25-1.25H14v-2.5a1.5 1.5 0 0 0-1.5-1.5h-.268a6.453 6.453 0 0 1-.684-2.202a6 6 0 0 0 2.946-1.822a5 5 0 0 0 4.503-5.152a.75.75 0 0 0-.552-.698A31.804 31.804 0 0 0 16 2.562v-.387a.75.75 0 0 0-.629-.74A33.227 33.227 0 0 0 10 1ZM2.525 4.422C3.012 4.3 3.504 4.19 4 4.09V5c0 .74.134 1.448.38 2.103a3.503 3.503 0 0 1-1.855-2.68Zm14.95 0a3.503 3.503 0 0 1-1.854 2.68A5.974 5.974 0 0 0 16 5v-.91c.496.099.988.21 1.475.332Z" clipRule="evenodd"></path>
           </svg>1st place
         </span>
       </div>
@@ -284,8 +290,8 @@ export default function Home() {
               <div>Discord Bot</div>
               <span className="py-0.5 px-2 font-medium text-gray-800 uppercase bg-gray-100 rounded-full text-[10px]">coming soon</span>
             </button> */}
-            <a href="#" target="_blank" rel="noreferrer" className="flex gap-x-2 items-center rounded-lg px-4 py-2 font-semibold text-black bg-white shadow">
-              <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" className="w-5 iconify iconify--logos" width="1em" height="1em" viewBox="0 0 256 256" style={{ color: "white" }}><defs><linearGradient id="iconifyReact2" x1="50%" x2="50%" y1="0%" y2="100%"><stop offset="0%" stop-color="#2AABEE"></stop><stop offset="100%" stop-color="#229ED9"></stop></linearGradient></defs><path fill="url(#iconifyReact2)" d="M128 0C94.06 0 61.48 13.494 37.5 37.49A128.038 128.038 0 0 0 0 128c0 33.934 13.5 66.514 37.5 90.51C61.48 242.506 94.06 256 128 256s66.52-13.494 90.5-37.49c24-23.996 37.5-56.576 37.5-90.51c0-33.934-13.5-66.514-37.5-90.51C194.52 13.494 161.94 0 128 0Z"></path><path fill="#FFF" d="M57.94 126.648c37.32-16.256 62.2-26.974 74.64-32.152c35.56-14.786 42.94-17.354 47.76-17.441c1.06-.017 3.42.245 4.96 1.49c1.28 1.05 1.64 2.47 1.82 3.467c.16.996.38 3.266.2 5.038c-1.92 20.24-10.26 69.356-14.5 92.026c-1.78 9.592-5.32 12.808-8.74 13.122c-7.44.684-13.08-4.912-20.28-9.63c-11.26-7.386-17.62-11.982-28.56-19.188c-12.64-8.328-4.44-12.906 2.76-20.386c1.88-1.958 34.64-31.748 35.26-34.45c.08-.338.16-1.598-.6-2.262c-.74-.666-1.84-.438-2.64-.258c-1.14.256-19.12 12.152-54 35.686c-5.1 3.508-9.72 5.218-13.88 5.128c-4.56-.098-13.36-2.584-19.9-4.708c-8-2.606-14.38-3.984-13.82-8.41c.28-2.304 3.46-4.662 9.52-7.072Z"></path></svg>
+            <a href={BOT_LINK} target="_blank" rel="noreferrer" className="flex gap-x-2 items-center rounded-lg px-4 py-2 font-semibold text-black bg-white shadow">
+              <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" className="w-5 iconify iconify--logos" width="1em" height="1em" viewBox="0 0 256 256" style={{ color: "white" }}><defs><linearGradient id="iconifyReact2" x1="50%" x2="50%" y1="0%" y2="100%"><stop offset="0%" stopColor="#2AABEE"></stop><stop offset="100%" stopColor="#229ED9"></stop></linearGradient></defs><path fill="url(#iconifyReact2)" d="M128 0C94.06 0 61.48 13.494 37.5 37.49A128.038 128.038 0 0 0 0 128c0 33.934 13.5 66.514 37.5 90.51C61.48 242.506 94.06 256 128 256s66.52-13.494 90.5-37.49c24-23.996 37.5-56.576 37.5-90.51c0-33.934-13.5-66.514-37.5-90.51C194.52 13.494 161.94 0 128 0Z"></path><path fill="#FFF" d="M57.94 126.648c37.32-16.256 62.2-26.974 74.64-32.152c35.56-14.786 42.94-17.354 47.76-17.441c1.06-.017 3.42.245 4.96 1.49c1.28 1.05 1.64 2.47 1.82 3.467c.16.996.38 3.266.2 5.038c-1.92 20.24-10.26 69.356-14.5 92.026c-1.78 9.592-5.32 12.808-8.74 13.122c-7.44.684-13.08-4.912-20.28-9.63c-11.26-7.386-17.62-11.982-28.56-19.188c-12.64-8.328-4.44-12.906 2.76-20.386c1.88-1.958 34.64-31.748 35.26-34.45c.08-.338.16-1.598-.6-2.262c-.74-.666-1.84-.438-2.64-.258c-1.14.256-19.12 12.152-54 35.686c-5.1 3.508-9.72 5.218-13.88 5.128c-4.56-.098-13.36-2.584-19.9-4.708c-8-2.606-14.38-3.984-13.82-8.41c.28-2.304 3.46-4.662 9.52-7.072Z"></path></svg>
               <div className="whitespace-nowrap">Telegram Bot</div>
             </a>
           </div>
@@ -302,13 +308,13 @@ export default function Home() {
       <div className="flex flex-col mt-32 lg:mt-5">
         <p className="mx-auto mb-2 text-3xl text-mochi">Integrated Chains</p>
         <div className="flex overflow-hidden py-3 w-20 h-20 marquee-container">
-          <div className="overlay" style={{"--gradient-color": "rgba(255, 255, 255, 1), rgba(255, 255, 255, 0)", "--gradient-width": "200px"} as React.CSSProperties}></div>
-          <div className="marquee" style={{"--play": "running", "--direction": "reverse", "--duration": "20s", "--delay": "0s", "--iteration-count": "infinite"} as React.CSSProperties}>
+          <div className="overlay" style={{ "--gradient-color": "rgba(255, 255, 255, 1), rgba(255, 255, 255, 0)", "--gradient-width": "200px" } as React.CSSProperties}></div>
+          <div className="marquee" style={{ "--play": "running", "--direction": "reverse", "--duration": "20s", "--delay": "0s", "--iteration-count": "infinite" } as React.CSSProperties}>
             <img className="h-full duration-100 ease-in-out mx-7 md:mx-10" src="/media/eth.png" alt="eth-logo" style={{ transitionProperty: "filter" }} />
             <img className="h-full duration-100 ease-in-out mx-7 md:mx-10" src="/media/arbitrum.png" alt="arbitrum-logo" style={{ transitionProperty: "filter" }} />
             <img className="h-full duration-100 ease-in-out mx-7 md:mx-10" src="/media/bsc.png" alt="bsc-logo" style={{ transitionProperty: "filter" }} />
           </div>
-          <div className="marquee" aria-hidden="true" style={{"--play": "running", "--direction": "reverse", "--duration": "20s", "--delay": "0s", "--iteration-count": "infinite"} as React.CSSProperties}>
+          <div className="marquee" aria-hidden="true" style={{ "--play": "running", "--direction": "reverse", "--duration": "20s", "--delay": "0s", "--iteration-count": "infinite" } as React.CSSProperties}>
             <img className="h-full duration-100 ease-in-out mx-7 md:mx-10" src="/media/eth.png" alt="eth-logo" style={{ transitionProperty: "filter" }} />
             <img className="h-full duration-100 ease-in-out mx-7 md:mx-10" src="/media/arbitrum.png" alt="arbitrum-logo" style={{ transitionProperty: "filter" }} />
             <img className="h-full duration-100 ease-in-out mx-7 md:mx-10" src="/media/bsc.png" alt="bsc-logo" style={{ transitionProperty: "filter" }} />
@@ -401,7 +407,7 @@ export default function Home() {
         <div className="px-6 md:px-12 body-block">
           <div style={{ backgroundImage: "url(/dot-bg.png), linear-gradient(90deg, #FCBCC8 0%, #D3A7F3 100%)", backgroundSize: "cover", backgroundBlendMode: "overlay", height: "300px" }} className="flex relative flex-col justify-center items-center p-10 rounded-lg">
             <p className="text-lg text-center md:text-3xl">Boost up your wealth. For Free ✨✨</p>
-            <a href="#" target="_blank" rel="noreferrer" className="flex gap-x-2 items-center rounded-lg px-2 py-2 font-semibold text-mochi bg-white mt-3">
+            <a href={BUY_LINK} target="_blank" rel="noreferrer" className="flex gap-x-2 items-center rounded-lg px-2 py-2 font-semibold text-mochi bg-white mt-3">
               <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 168.3 193.8" style={{ enableBackground: "new 0 0 168.3 193.8" } as React.CSSProperties} xmlSpace="preserve">
                 <path className="st0" d="M66,44.1c-2.1-0.3-2.2-0.4-1.2-0.5c1.9-0.3,6.3,0.1,9.4,0.8c7.2,1.7,13.7,6.1,20.6,13.8l1.8,2.1l2.6-0.4  c11.1-1.8,22.5-0.4,32,4c2.6,1.2,6.7,3.6,7.2,4.2c0.2,0.2,0.5,1.5,0.7,2.8c0.7,4.7,0.4,8.2-1.1,10.9c-0.8,1.5-0.8,1.9-0.3,3.2  c0.4,1,1.6,1.7,2.7,1.7c2.4,0,4.9-3.8,6.1-9.1l0.5-2.1l0.9,1c5.1,5.7,9.1,13.6,9.7,19.2l0.2,1.5l-0.9-1.3c-1.5-2.3-2.9-3.8-4.8-5.1  c-3.4-2.3-7-3-16.5-3.5c-8.6-0.5-13.5-1.2-18.3-2.8c-8.2-2.7-12.4-6.2-22.1-19.1c-4.3-5.7-7-8.8-9.7-11.4  C79.6,48.3,73.7,45.3,66,44.1z" />
                 <path className="st0" d="M140.5,56.8c0.2-3.8,0.7-6.3,1.8-8.6c0.4-0.9,0.8-1.7,0.9-1.7c0.1,0-0.1,0.7-0.4,1.5c-0.8,2.2-0.9,5.3-0.4,8.8  c0.7,4.5,1,5.1,5.8,10c2.2,2.3,4.8,5.2,5.8,6.4l1.7,2.2l-1.7-1.6c-2.1-2-6.9-5.8-8-6.3c-0.7-0.4-0.8-0.4-1.3,0.1  c-0.4,0.4-0.5,1-0.5,3.9c-0.1,4.5-0.7,7.3-2.2,10.2c-0.8,1.5-0.9,1.2-0.2-0.5c0.5-1.3,0.6-1.9,0.6-6.2c0-8.7-1-10.8-7.1-14.3  c-1.5-0.9-4.1-2.2-5.6-2.9c-1.6-0.7-2.8-1.3-2.7-1.3c0.2-0.2,6.1,1.5,8.4,2.5c3.5,1.4,4.1,1.5,4.5,1.4  C140.2,60.1,140.4,59.3,140.5,56.8z" />
